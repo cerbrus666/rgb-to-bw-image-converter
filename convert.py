a4fe354d705e4e6e268b1073ae6e1493dccdd508
Shukor, Halim <shukor.h@duerr-ndt.com>
2023-06-30 11:07:15 +0800

    fix

--- convert.py
+++ convert.py
@@ -11,8 +11,8 @@ def colour_to_gray(file_path, output_path):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-i", "--input", help="3 channel rgb input image path to be converted"
+        "-i", "--input", help="3-channels rgb input image path to be converted"
     )
-    parser.add_argument("-o", "--output", help="1 channel output image path result")
+    parser.add_argument("-o", "--output", help="1-channel output image path result")
     args = parser.parse_args()
     colour_to_gray(args.input, args.output)
