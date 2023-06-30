10aa8a1365ae3185677bda9d2e1e51fa433b70c8
Shukor, Halim <shukor.h@duerr-ndt.com>
2023-06-30 10:47:35 +0800

    basic script

---
+++ convert.py
@@ -0,0 +1,18 @@
+import cv2
+import argparse
+
+
+def colour_to_gray(file_path, output_path):
+    image_mat = cv2.imread(file_path, flags=cv2.IMREAD_COLOR)
+    img_gray = cv2.cvtColor(image_mat, cv2.COLOR_BGR2GRAY)
+    status = cv2.imwrite(output_path, img_gray)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-i", "--input", help="3 channel rgb input image path to be converted"
+    )
+    parser.add_argument("-o", "--output", help="1 channel output image path result")
+    args = parser.parse_args()
+    colour_to_gray(args.input, args.output)
